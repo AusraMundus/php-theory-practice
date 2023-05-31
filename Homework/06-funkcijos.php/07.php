b996344b6fe71a91a05c725bf408c06e16f09d85
Ausra <munduswebstudio@gmail.com>
2023-05-31 10:21:36 +0300

    Functions Homework

--- Homework/06-funkcijos.php/07.php
+++ Homework/06-funkcijos.php/07.php
@@ -6,31 +6,21 @@ echo '____7 užduotis___<br><br>';
 
 // 7. Sugeneruokite atsitiktinio (nuo 10 iki 20) ilgio masyvą, kurio visi, išskyrus paskutinį, elementai yra atsitiktiniai skaičiai nuo 0 iki 10, o paskutinis masyvas, kuris generuojamas pagal tokią pat salygą kaip ir pirmasis masyvas. Viską pakartokite atsitiktinį nuo 10 iki 30  kiekį kartų. Paskutinio masyvo paskutinis elementas yra lygus 0;
 
-function mainArray() {
-    $arrayLength = rand(10, 20);
-    $array = [];
-    for ($i = 0; $i < $arrayLength - 1; $i++) {
-        $subArray = rand(0, 10);
-        $array[] = $subArray;
+function makeArray($maxCount) {
+    static $counter = 0;
+    $subArray = [];
+    for($i = 1; $i <= 9; $i++){
+        $subArray[]= rand(0, 10);
     }
-    return $array;
-}
-
-function lastSubArray() {
-    $subArray = mainArray();
-    $subArray[] = 0;
-    return $subArray;
-}
-
-$rounds = rand(10, 30);
-for ($i = 0; $i < $rounds; $i++) {
-    if ($i == $rounds - 1) {
-        $array = lastSubArray();
+    $counter++;
+    if ($counter < $maxCount){
+        $subArray[] = makeArray($maxCount);
     } else {
-        $array = mainArray();
+        $subArray[] = 0;
     }
-    echo "Masyvas $i: ";
-    print_r($array);
-    echo '<br>';
+return $subArray;
 }
 
+$array[] = makeArray(rand(10,30));
+
+print_r($array);
