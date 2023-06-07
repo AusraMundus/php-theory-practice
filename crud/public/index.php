79b1957b3f5c6815033ceedd2c68b70e9d25d621
Ausra <munduswebstudio@gmail.com>
2023-06-07 14:31:33 +0300

    CRUD. Color API

--- crud/public/index.php
+++ crud/public/index.php
@@ -1,3 +1,10 @@
+<?php
+
+$colors = file_get_contents(__DIR__ . '/../colors.ser');
+$colors = $colors ? unserialize($colors) : [];
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -12,6 +19,31 @@
 
 <body>
     <?php require __DIR__ . '/menu.php' ?>
+
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-9">
+                <div class="card m-5">
+                    <div class="card-header">
+                        <h2>Colors list</h2>
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <?php foreach ($colors as $color) : ?>
+                            <li class="list-group-item">
+                                <div class="bin">
+                                    <div class="color-block" style="background:<?= $color['color'] ?>;"><?= $color['name'] ?></div>
+                                    <form action="./delete.php?id=<?= $color['id'] ?>" method="post">
+                                        <button type="submit" class="delete"></button>
+                                    </form>
+                                    <a href="./edit.php?id=<?= $color['id'] ?>" class="edit"></a>
+                                </div>
+                            </li>
+                        <?php endforeach ?>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
 
 </body>
 
