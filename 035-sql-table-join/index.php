d5c9fa777ab870f0b9caf5d51d5ce89a4bfbf100
Ausra <munduswebstudio@gmail.com>
2023-07-03 11:35:46 +0300

    SQL DB join

---
+++ 035-sql-table-join/index.php
@@ -0,0 +1,114 @@
+<?php
+
+$host = 'localhost';
+$db   = 'studies';
+$user = 'root';
+$pass = '';
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+
+$pdo = new PDO($dsn, $user, $pass, $options);
+
+// JOIN budai - duombaze apie juos nezino. Duombazeje reikia sujungti papildomai
+
+// INNER JOIN
+$sql = "
+    SELECT c.id, name, number
+    FROM clients AS c
+    INNER JOIN phone AS p
+    ON c.id = p.client_id
+";
+// pervadiname lenteles, sutrumpiname
+// ON - taisykle, kaip apjungti DB lenteles
+// SELECT * - su zvaigzdute reiskia ALL
+// SELECT - nurodom, kas is kur paimta.
+
+$stmt = $pdo->query($sql); // DB steitmentas
+$clients = $stmt->fetchAll(); // Visko istraukimas
+
+?>
+
+<h1>INNER JOIN</h1>
+<table>
+    <tr>
+        <th>ID</th>
+        <th>Name</th>
+        <th>Phone</th>
+    </tr>
+    <?php foreach ($clients as $client) : ?>
+        <tr>
+            <td><?= $client['id'] ?></td>
+            <td><?= $client['name'] ?></td>
+            <td><?= $client['number'] ?></td>
+        </tr>
+    <?php endforeach ?>
+</table>
+
+
+<?php
+
+// LEFT JOIN
+$sql = "
+    SELECT c.id, name, number
+    FROM clients AS c
+    LEFT JOIN phone AS p
+    ON c.id = p.client_id
+";
+
+$stmt = $pdo->query($sql); // DB steitmentas
+$clients = $stmt->fetchAll(); // Visko istraukimas
+
+?>
+
+<h1>LEFT JOIN</h1>
+<table>
+    <tr>
+        <th>ID</th>
+        <th>Name</th>
+        <th>Phone</th>
+    </tr>
+    <?php foreach ($clients as $client) : ?>
+        <tr>
+            <td><?= $client['id'] ?></td>
+            <td><?= $client['name'] ?></td>
+            <td><?= $client['number'] ?></td>
+        </tr>
+    <?php endforeach ?>
+</table>
+
+<?php
+
+// RIGHT JOIN
+$sql = "
+    SELECT c.id, name, number
+    FROM clients AS c
+    RIGHT JOIN phone AS p
+    ON c.id = p.client_id
+";
+
+$stmt = $pdo->query($sql); // DB steitmentas
+$clients = $stmt->fetchAll(); // Visko istraukimas
+
+?>
+
+<h1>RIGHT JOIN</h1>
+<table>
+    <tr>
+        <th>ID</th>
+        <th>Name</th>
+        <th>Phone</th>
+    </tr>
+    <?php foreach ($clients as $client) : ?>
+        <tr>
+            <td><?= $client['id'] ?></td>
+            <td><?= $client['name'] ?></td>
+            <td><?= $client['number'] ?></td>
+        </tr>
+    <?php endforeach ?>
+</table>
